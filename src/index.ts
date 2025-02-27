9de74fde4470d9839600148d0ead524b87a750d4
cindytsai <turquoisea.tsai@gmail.com>
2025-02-27 00:10:55 -0600

    Make sure frontend have effect in package.

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ import {
 } from '@jupyterlab/application';
 
 /**
- * Initialization data for the @jupyterlab-examples/hello-world extension.
+ * Initialization data for the jupyter-libyt extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'jupyter-libyt/frontend:plugin',
+  id: 'jupyter-libyt:plugin',
   description: 'A JupyterLab extension for libyt.',
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
