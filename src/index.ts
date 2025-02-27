f4311efa3435132a11d5e62386cf35f7e465a8f3
cindytsai <turquoisea.tsai@gmail.com>
2025-02-27 17:39:16 -0600

    Add behavior to interrupt kernel button in toolbar. (Worked)

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,14 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+import { INotebookTracker } from '@jupyterlab/notebook';
+import { Notification } from '@jupyterlab/apputils';
+import { stopIcon } from '@jupyterlab/ui-components';
+
+
+const CommandIds = {
+  Interrupt: 'jupyter-libyt:interrupt'
+}
 
 /**
  * Initialization data for the jupyter-libyt extension.
@@ -10,8 +18,27 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyter-libyt:plugin',
   description: 'A JupyterLab extension for libyt.',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [INotebookTracker],
+  activate: (app: JupyterFrontEnd, tracker: INotebookTracker) => {
     console.log("[jupyter_libyt] frontend extension activated", app);
+
+    const { commands } = app;
+
+    // Add behavior to interrupt
+    commands.addCommand(CommandIds.Interrupt, {
+      label: 'Interrupt',
+      caption: 'Interrupt the kernel',
+      icon: stopIcon,
+      execute: () => {
+        // Libyt Kernel
+        let kernel_name = tracker.currentWidget?.context.sessionContext?.session?.kernel?.name;
+        if (kernel_name === 'libyt_kernel') {
+          Notification.warning('Interrupt is disabled in jupyter libyt', {autoClose: 3000});
+        } else {
+          commands.execute("kernelmenu:interrupt");
+        }
+      }
+    });
   }
 };
 
